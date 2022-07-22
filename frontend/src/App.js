14087da8d65dcece38de9aeaa9db41cc85ddecce
Bikram Roy <bikramroy364@gmail.com>
2022-07-22 21:03:30 +0530

    Install Basic Lib & Setup Basic Pages and Mongoose Models
    Frontend
    
    -> Add backend url to frontend proxy
    -> Install MaterialUI with emotion, Axios, React Router Dom
    -> Setup router with basic ChatPage, HomePage
    
    Backend
    -> Install Mongoose
    -> Create chatModel, messageModel, userModel in mongoose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage/HomePage';
+import Container from '@mui/material/Container';
+import ChatPage from './Pages/ChatPage/ChatPage';
 
 function App() {
-  return (
-    <div className="App">
-      Home
-    </div>
-  );
+    return (
+        <Container
+            maxWidth='lg'
+            sx={{
+                bgcolor: '#cfe8fc',
+                height: '100vh',
+            }}
+        >
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/chat' element={<ChatPage />} />
+            </Routes>
+        </Container>
+    );
 }
 
 export default App;
